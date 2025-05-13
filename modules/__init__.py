77e01506e04718c4003b8a0f6d84d90a38e1a8cf
Cerax <166728989+sehraks@users.noreply.github.com>
2025-05-13 14:17:12 +0800

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,21 +1,15 @@
-"""
-Facebook MonoToolkit Package
---------------------------
-This package contains modules for Facebook automation tasks including:
-- Cookie management
-- Spam sharing functionality
-- Utility functions
-"""
+# modules/__init__.py
 
+from .utils import Utils
 from .cookie_manager import CookieManager
 from .spam_sharing import SpamSharing
-from .utils import clear_screen, validate_input
 
-__version__ = "1.0.0"
-__author__ = "sehraks"
-__all__ = [
-    'CookieManager',
-    'SpamSharing',
-    'clear_screen',
-    'validate_input'
-]
+# Create convenience functions if needed
+def clear_screen():
+    return Utils.clear_screen()
+
+def validate_input(*args, **kwargs):
+    return Utils.validate_input(*args, **kwargs)
+
+# Export the classes and functions
+__all__ = ['Utils', 'CookieManager', 'SpamSharing', 'clear_screen', 'validate_input']
